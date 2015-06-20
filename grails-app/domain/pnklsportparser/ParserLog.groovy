42c8a3e3c7ba49e1d77a97ed038033b72b85f1d3
evgeniy <evgeniy@takeit.to>
2015-06-20 20:28:45 +0300

    create domain class ParserLog

---
+++ grails-app/domain/pnklsportparser/ParserLog.groovy
@@ -0,0 +1,30 @@
+package pnklsportparser
+
+class ParserLog {
+    Date createdDate
+    Integer fixtureNum
+    Integer oddNum
+    String sportId
+    String leaguesIds
+    String status
+
+    
+    static constraints = {
+        createdDate(nullable:false, blank: false)
+        fixtureNum(nullable:false, blank: false)
+        oddNum(nullable:false, blank: false)
+        sportId(nullable:false, blank: false)
+        leaguesIds(nullable:false, blank: false)
+        status(nullable:false, blank: false)
+    }
+    
+    static mapping = {
+        version false
+        createdDate column: 'createdDate'
+        fixtureNum column: 'fixtureNum'
+        oddNum column: 'oddNum'
+        sportId column: 'sportId'
+        leaguesIds column: 'leaguesIds'
+        status column: 'status'
+    }
+}
